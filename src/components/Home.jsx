e6b36661f882946f2cff358b773cae583dc6914b
Shivam Yadav <154537760+shivamy009@users.noreply.github.com>
2024-09-16 09:36:16 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <h1 className="text-5xl font-bold mb-4">Connect with People</h1>
         <p className="text-lg mb-8">Discover new friends and build meaningful connections.</p>
         {/* <button className="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-blue-100 hover:text-white">Join Now</button> */}
-        <Link  className="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-blue-100 hover:text-white" to={access_token ? '/dashboard' :'/login'}>
+        <Link  className="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-blue-100 hover:text-white" to={access_token ? '/dashboard' :'/signup'}>
         Join Now
         </Link>
       </div>
